985c95231daf5c77ce66cd154c430acbc9a2ad4c
Prashant rawal <Prashantrawal619@gmail.com>
2019-11-25 17:48:39 +0530

    add events component statically

--- src/EventDashboard.jsx
+++ src/EventDashboard.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
+import EventList from "./EventList";
 class EventDashboard extends Component {
   render() {
     return (
       <Grid>
-        <Grid.Column width={10}>left column</Grid.Column>
+        <Grid.Column width={10}>
+          <EventList />
+        </Grid.Column>
         <Grid.Column width={6}>right column</Grid.Column>
       </Grid>
     );
---
+++ src/EventListAttendee.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { List, Image } from "semantic-ui-react";
+class EventListAttendee extends Component {
+  render() {
+    return (
+      <List.Item>
+        <Image
+          as="a"
+          size="mini"
+          circular
+          src="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </List.Item>
+    );
+  }
+}
+
+export default EventListAttendee;

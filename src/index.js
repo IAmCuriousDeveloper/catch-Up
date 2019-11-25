5f8ead8ebc6beff65ccf9474bae2e70bb8b5be88
Prashant rawal <Prashantrawal619@gmail.com>
2019-11-25 11:35:34 +0530

    initial commit add hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { tsModuleBlock } from "@babel/types";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById("root");
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
